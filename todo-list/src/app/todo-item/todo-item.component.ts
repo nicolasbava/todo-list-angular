52b068977f78eab6eb745b1ba9d72b529f90d04e
nicolasbava <nicobava96@gmail.com>
2022-09-05 19:01:50 -0300

    feat: add interface todoItem to the appç

---
+++ todo-list/src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() item: TodoItemItem;
+
+  ngOnInit(): void {
+  }
+
+}
